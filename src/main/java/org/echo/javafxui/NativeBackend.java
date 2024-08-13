00f4f8df692af6294a4f8979e1eaaed1b0de0347
mineshadow <shad-karagodin@ya.ru>
2024-08-13 14:22:38 +0300

    small fixes

--- src/main/java/org/echo/javafxui/NativeBackend.java
+++ src/main/java/org/echo/javafxui/NativeBackend.java
@@ -4,7 +4,7 @@ public class NativeBackend {
     static {
         try {
             // Load the shared library from a specific path
-            System.load("/home/mineshadow/IdeaProjects/JavaFXUI/src/main/cpp/build/lib/libbackend.so");
+            System.load("/home/mineshadow/IdeaProjects/MultiLangProject/src/main/cpp/build/lib/libbackend.so");
         }catch (Exception e){
             System.err.println("Failed to load libbackend.so. Check if the backend src/main/cpp is built and you are using Linux system.");
         }
